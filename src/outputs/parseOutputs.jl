b020c874490811189894f93f2154ded70477d027
louislat <lmm.latournerie@gmail.com>
2025-03-02 10:00:26 +0100

    add impact_in/out in output

--- src/outputs/parseOutputs.jl
+++ src/outputs/parseOutputs.jl
@@ -6,7 +6,12 @@ function generateMarginalEffectsOutput(∂S∂Wab::Matrix, ∂S∂Wba::Matrix, n
     user_impact_in = vec(sum(∂S∂Wab, dims = 1))
     user_impact_out = vec(sum(∂S∂Wba, dims = 2))
     user_overall_impact = user_impact_in .+ user_impact_out
-    output::DataFrame = DataFrame(id = 1:length(user_overall_impact), impact = user_overall_impact)
+    output::DataFrame = DataFrame(
+        id = 1:length(user_overall_impact),
+        impact_in = user_impact_in,
+        impact_out = user_impact_out,
+        impact = user_overall_impact,
+    )
     output = leftjoin(output, nodes_id[nodes_id.cluster .== 0, :], on = :id)
     
     return select!(output, :node, :id, :impact)
