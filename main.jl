f83ec15c54f9144f3da46a51d62163f5ff0f230d
louislat <lmm.latournerie@gmail.com>
2025-03-02 10:01:55 +0100

    format main

--- main.jl
+++ main.jl
@@ -7,6 +7,9 @@ include("src/debtRank.jl")
 include("src/outputs/parseOutputs.jl")
 
 
+# impacts = CSV.read("sample_data/impacts.csv", DataFrame)
+# nodes_info = CSV.read("sample_data/nodes_info.csv", DataFrame)
+
 impacts = DataFrame(
     source = ["A", "A", "d", "e"],
     target = ["d", "e", "B", "C"],
@@ -23,36 +26,6 @@ impacted_node = "A"
 shock = 0.1
 
 simulation = DebtRankLstmSimulation(impacts, nodes_info, 5)
-
-# println(simulation.nodes_id)
-# println(simulation.lstm.Wab)
-# println(simulation.lstm.Wba)
-
-# impacted_node_cluster = simulation.nodes_id[
-#     simulation.nodes_id.node .== impacted_node, :
-# ].cluster[1]
-# impacted_node_id = simulation.nodes_id[
-#     simulation.nodes_id.node .== impacted_node, :
-# ].id[1]
-
-# nodes_id_a = simulation.nodes_id[simulation.nodes_id.cluster .== 1, :]
-# nodes_id_b = simulation.nodes_id[simulation.nodes_id.cluster .== 0, :]
-
-# na = maximum(nodes_id_a.id)
-# nb = maximum(nodes_id_b.id)
-
-# weight_a = sort!(nodes_id_a, :id).weight
-# weight_b = sort!(nodes_id_b, :id).weight
-
-# ca, ha, cb, hb = zeros(na), zeros(na), zeros(nb), zeros(nb)
-# if impacted_node_cluster == 1
-#     ha[impacted_node_id] = shock
-# else
-#     hb[impacted_node_id] = shock
-# end
-
-
-# forward(simulation, ca, ha, cb, hb, weight_a, weight_b)
 
 ∂S∂Wab, ∂S∂Wba = getMarginalEffects(simulation, impacted_node, shock)
 
